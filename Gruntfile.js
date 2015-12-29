1fbc02cb5a4959c4fe3c79ea391b3708e366d829
Lee Bock <lbock@esri.com>
2015-12-29 12:03:29 -0500

    added task management stuff

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+'use strict';
+var packagejson = require('./package.json');
+
+module.exports = function (grunt) {
+ 
+  // Configuration
+
+  grunt.initConfig({
+    pkg: packagejson,
+    jshint: {
+      build: [
+        'js/*.js'
+      ],
+      options: {jshintrc: '.jshintrc', ignores:[]}
+    },
+    watch: {
+      scripts: {
+        files: ['index.html','js/*.js','css/*.css'],
+        tasks: ['jshint'],
+        options: {
+          debounceDelay: 250,
+          livereload: true
+        },
+      },
+    }        
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint','watch']);
+  
+};
