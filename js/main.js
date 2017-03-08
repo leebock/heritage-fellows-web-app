c0e45bc140bce524ef02b2718697ac5b9d076935
leebock <lbock@esri.com>
2017-03-08 14:46:43 -0500

    added some marker click basics

--- js/main.js
+++ js/main.js
@@ -19,7 +19,9 @@
 			.addLayer(L.esri.basemapLayer("DarkGray"))
 			.addLayer(L.esri.basemapLayer("DarkGrayLabels"));
 
-		_layerDots = L.featureGroup().addTo(_map);
+		_layerDots = L.featureGroup()
+			.addTo(_map)
+			.on("click", onMarkerClick);
 
 		Papa.parse(
 			SPREADSHEET_URL, 
@@ -43,7 +45,7 @@
 						)
 						.addTo(_layerDots);
 					if (!L.Browser.mobile) {
-						marker.bindTooltip(value[FIELDNAME$STANDARDIZED_LOCATION].split(",")[0])
+						marker.bindTooltip(value[FIELDNAME$STANDARDIZED_LOCATION].split(",")[0]);
 					}
 				}
 			);
@@ -51,5 +53,55 @@
 		}
 
 	});
+
+	function onMarkerClick(e)
+	{
+
+		// so this doesn't further tricker map::click
+
+    	L.DomEvent.stop(e);
+
+		/* note: 
+
+			because of weirdness with toolTip, currently you need to manually 
+			hide the toolTip so that it doesn't display concurrently with the
+			popup (which looks weird).
+		*/
+
+		$(".leaflet-tooltip").remove();
+
+		/*
+
+			further note: in the past we have had to put the remove inside a 
+			one-time movestart event AND we have also had to put the pan on a
+			slight time delay (setTimeout).  just sayin'...
+
+		    _map.once('movestart', function(e) {
+
+		    });
+
+		*/
+
+		_map.panTo(e.layer.getLatLng());
+
+		/* 	final note: 
+
+			code for situating the map w/ offsets. using a test offset here, 
+		 	but the offset would generally be the height or width of some 
+		 	container.
+
+			var pixels = _map.latLngToContainerPoint(e.layer.getLatLng());
+			var offset = 0; // height or width of some container.
+
+			if ($("html body").hasClass("mobile")) {
+				pixels = pixels.add([0, offset/2]);  // vertical offset
+			} else {
+				pixels = pixels.add([-offset/2, 0]); // left hand offset
+			}
+			_map.panTo(_map.containerPointToLatLng(pixels), {animate: true, duration: 1});					
+
+		*/
+
+	}
 
 })();
