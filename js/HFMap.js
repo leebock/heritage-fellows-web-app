f81cd402c67d32c8fe7cf8dc6d016ac6bef1402f
leebock <lbock@esri.com>
2018-06-14 14:36:44 -0400

    eliminated SummaryTable class and moved summarizing logic into HFMap...might be a mistake now that I think of it.  Because I may end up having a second highlight layer.  But I'll cross that bridge when I get to it.

--- js/HFMap.js
+++ js/HFMap.js
@@ -47,7 +47,7 @@ L.HFMap = L.Map.extend({
 
     layerDots.clearLayers();
 
-    var sumTable = new SummaryTable().createSummaryTable(recs);
+    var sumTable = this._createSummary(recs);
 
     var marker, frequency;
     $.each(
@@ -79,10 +79,63 @@ L.HFMap = L.Map.extend({
       }
     );
 
-  }
+  },
 
   /*************************************************/
   /************* "PRIVATE" FUNCTIONS ***************/
   /*************************************************/
+
+  _createSummary: function(recs)
+  {
+
+    var uniques = $.map(
+      recs, 
+      function(value, index){return value.getStandardizedLocation();}
+    )
+      .sort()
+      .reduce(
+        function(accumulator, current) {
+            const length = accumulator.length;
+            if (length === 0 || accumulator[length - 1] !== current) {
+                accumulator.push(current);
+            }
+            return accumulator;
+        }, 
+        []
+      );
+
+
+    var table = [];
+    var selected;
+
+    $.each(
+      uniques, 
+      function(index, value) {
+
+        selected = $.grep(
+          recs, 
+          function(rec, idx) {
+            return rec.getStandardizedLocation() === value;
+          }
+        );
+
+        table.push(
+          new SummaryRecord(
+            value, 
+            selected[0].getLatLng(),
+            selected[0].getLocationDisplayName(),
+            selected.length,
+            selected[0].getLastName()           
+          )
+        );
+
+      }
+    );
+
+    table.sort(function(a, b){return b.getFrequency()-a.getFrequency();});
+
+    return table;
+
+  }
 
 });
