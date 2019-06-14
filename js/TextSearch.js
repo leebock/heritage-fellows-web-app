44b764d3dae2e0059662316ca2f234908109f0d4
Lee Bock <lbock@esri.com>
2019-06-14 15:14:08 -0400

    TextSearch: don't fire change when the value hasn't changed, duh.

--- js/TextSearch.js
+++ js/TextSearch.js
@@ -1,6 +1,7 @@
 function TextSearch()
 {
 
+	this._lastValue = "";
 	var _self = this;
 
 	$("#search input").on("keyup", onInputKeyUp);	
@@ -9,18 +10,23 @@ function TextSearch()
 	function onInputKeyUp()
 	{
 		var value = $("#search input").val();
+		if (value === _self._lastValue) {
+			return;
+		}
 		if ($.trim(value).length) {
 			$("#search .x-button").css("visibility", "visible");
 		} else {
 			$("#search .x-button").css("visibility", "hidden");
 		}
+		_self._lastValue = value;
 		$(_self).trigger("change", [value]);
 	}
 
 	function onClear()
 	{
 		$("#search .x-button").css("visibility", "hidden");
-		$("#search input").val("");	
+		$("#search input").val("");
+		_self._lastValue = "";	
 		$(_self).trigger("clear");
 	}
 
@@ -29,4 +35,5 @@ function TextSearch()
 TextSearch.prototype.clear = function() {
 	$("#search .x-button").css("visibility", "hidden");
 	$("#search input").val("");	
+	this._lastValue = "";	
 };
